2b46eb5224945dd64fec69666d3d76c1e673f779
ajaydewari <ajdewari@gmail.com>
2020-05-16 22:10:28 +0530

    initial commit

---
+++ app/src/main/java/com/dewari/ajay/mvvm_clean_architecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dewari.ajay.mvvm_clean_architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
