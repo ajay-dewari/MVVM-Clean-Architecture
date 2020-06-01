ab2f564397b56a550ca26408d1db82db290f3740
ajaydewari <ajdewari@gmail.com>
2020-06-02 00:00:25 +0530

    added Note entity

--- data/src/main/java/me/dewari/ajay/data/entity/Note.kt
+++ data/src/main/java/me/dewari/ajay/data/entity/Note.kt
@@ -3,4 +3,4 @@ package me.dewari.ajay.data.entity
 import androidx.room.Entity
 
 @Entity(tableName = "note_table")
-data class Note(val title: String, val description: String, val priority: String)
+data class Note(val title: String, val description: String, val priority: String){}
